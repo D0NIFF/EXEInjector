6c1a261c8369ac936025307a6a3f717ce128f64a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-09-29 15:51:22 +0300

    Update for injector

--- input_exe.py
+++ input_exe.py
@@ -1,4 +1,4 @@
-def input_exe():
+def input_exe(jpeg_path, exe_path, output_path):
 
     # Read JPEG file
     with open(jpeg_path, 'rb') as jpeg:
@@ -18,3 +18,11 @@ def input_exe():
     # The record union data in the output file
     with open(output_path, 'wb') as outfile:
         outfile.write(union_content)
+
+
+jpeg_file = 'image.jpg';    # Path to JPEG file
+exe_file = 'program.exe';   # Path to EXE file
+output_file = 'image2.jpg'; # Name for the output file
+
+input_exe(jpeg_file, exe_file, output_file)
+print(f"EXE inject inside JPEG file. Result: {output_file}")
